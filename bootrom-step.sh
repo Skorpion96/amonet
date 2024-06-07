5ecd268893a81b15865ba26e7eea36d6ba47e297
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2024-06-07 22:43:07 +0200

    Update bootrom-step.sh

--- bootrom-step.sh
+++ bootrom-step.sh
@@ -2,10 +2,10 @@
 
 set -e
 sudo apt install git && git clone https://github.com/bkerler/mtkclient.git && cd mtkclient && sudo apt install pip3 && pip3 install -r requirements.txt && pip3 install .
-read -p "Disconnect battery, disconnect the device and reconnect it in brom shorting click in the motherboard using a paperclip. Press [Enter] key to continue..."
+read -p "Disconnect battery, disconnect the device and reconnect it in brom shorting click in the motherboard using a paperclip. If it is detected as /dev/ttyACM0 Press [Enter] key to continue... Else repeat this step"
 #Next command is just to trigger mtkclient which is what matters to us, it won't really unlock seccfg, we need it to bypass watchdog protection
 python3 mtk da seccfg unlock
 cd modules
-read -p "Now Connect battery, disconnect the device and reconnect it in brom shorting click in the motherboard using the paperclip. Press [Enter] key to continue..."
+read -p "Now Connect battery, disconnect the device and reconnect it in brom shorting click in the motherboard using the paperclip. If it is detected as /dev/ttyACM0 Press [Enter] key to continue... Else repeat this step"
 python3 main.py
 cd ..
