64d96710ce87bb8a6bd799706d49e0a8a950e878
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2024-06-07 22:21:07 +0200

    Update bootrom-step.sh

--- bootrom-step.sh
+++ bootrom-step.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 set -e
-
+sudo apt install git && git clone https://github.com/bkerler/mtkclient.git && cd mtkclient && sudo apt install pip3 && pip3 install -r requirements.txt && pip3 install .
+read -p "Disconnect battery, disconnect the device and reconnect it in brom shorting click in the motherboard using a paperclip. Press [Enter] key to continue..."
+python3 mtk da seccfg unlock
 cd modules
+read -p "Now Connect battery, disconnect the device and reconnect it in brom shorting click in the motherboard using the paperclip. Press [Enter] key to continue..."
 python3 main.py
 cd ..
